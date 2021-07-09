2f5ccb00c22e9b60e83ca6677d245e431717815c
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-07-09 11:26:11 +0530

    Service-BusinessLogic is Done

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+import { Todo } from '../model/Todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        id: '111',
+        title: 'Learn C++',
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: '222',
+        title: 'Learn React',
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: '333',
+        title: 'Learn Angular',
+        isCompleted: false,
+        date: new Date(),
+      },
+    ];
+  }
 
-  constructor() { }
+  getTodos() {
+    return of(this.todos);
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  changeStatus(todo: Todo) {
+    this.todos.map((singleTodo) => {
+      if (singleTodo.id == todo.id) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const indexOfTodo = this.todos.findIndex(
+      (currentObj) => currentObj.id === todo.id
+    );
+    this.todos.splice(indexOfTodo, 1);
+  }
 }
